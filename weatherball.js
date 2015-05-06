c739e15c5d6fb006eaf8105097201007e6319211
Serge Nasr <nasrserge@gmail.com>
2015-05-06 14:01:20 -0400

    Added globe

--- weatherball.js
+++ weatherball.js
@@ -1,12 +1,21 @@
 if (Meteor.isClient) {
   // This code only runs on the client
-  Template.body.helpers({
-    cities: [
-      { name: "This is city 1" },
-      { name: "This is city 2" },
-      { name: "This is city 3" }
-    ]
-  });
+  Template.globe.rendered = function() {
+    var earth = new WE.map('globe-cv');
+    
+   WE.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
+          subdomains: '1234',
+          attribution: 'Tiles Courtesy of MapQuest'
+        }).addTo(earth);
+        
+    var marker = WE.marker([51.5, -0.09]).addTo(earth);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
+
+    var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
+    marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
+
+    earth.setView([51.505, 0], 6);
+  }
 }
 
 if (Meteor.isServer) {
